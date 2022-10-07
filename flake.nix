0d74d688ce78451dc2168ab25b29e485c8543538
Kasper Gałkowski <kgalkowski@comscore.com>
2022-10-07 21:28:18 +0200

    buduję clojure zbootstrapowanym antem

--- flake.nix
+++ flake.nix
@@ -9,8 +9,8 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.outputs.legacyPackages.${system};
     in {
-      packages.${system} = {
-        clojure = pkgs.callPackage ./clojure.nix {};
+      packages.${system} = rec {
+        clojure = pkgs.callPackage ./clojure.nix { inherit ant; };
         sbcl = pkgs.callPackage ./sbcl.nix {};
         emacs = pkgs.callPackage ./emacs.nix {};
         ant = pkgs.callPackage ./ant.nix {};
