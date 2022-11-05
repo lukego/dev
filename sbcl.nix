4bc1bdc637f5212a4f5220e91601b73d688837de
Kasper Gałkowski <kgalkowski@comscore.com>
2022-11-05 15:47:44 +0100

    Revert "bump sbcl do 2.2.10"
    This reverts commit b7d6b83d3c2f0c75a1dbb3d2d0c0be909fdc9397.

--- sbcl.nix
+++ sbcl.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
 
   pname = "sbcl";
 
-  version = "2.2.10";
+  version = "2.2.9";
 
   buildInputs = let
     zstd' = zstd.override { static = true; };
@@ -16,12 +16,8 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://sourceforge/project/sbcl/sbcl/${version}/sbcl-${version}-source.tar.bz2";
-    hash = "sha256-jMPDqHYSI63vFEqIcwsmdQg6Oyb6FV1wz5GruTXpCDM";
+    hash = "sha256-fr69bSAj//cHewNy+hFx+IBSm97GEE8gmDKXwv63wXI=";
   };
-
-  patches = [
-    ./patches/sbcl-dont-cat.patch
-  ];
 
   postPatch = ''
     echo '"${version}.kaspi"' > version.lisp-expr
