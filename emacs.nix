867f93812e63ef7ca028e30fded8f516d68c5c8f
Kasper Gałkowski <k@galkowski.xyz>
2022-12-02 11:17:55 +0100

    emacs do tip of trunk i gtk3->lucid

--- emacs.nix
+++ emacs.nix
@@ -4,7 +4,7 @@ let
 
   emacs = pkgs.emacs.override {
     inherit sqlite;
-    withGTK3 = true;
+    # withGTK3 = true;
     withSQLite3 = true;
     nativeComp = false; # Takes too long
   };
@@ -14,8 +14,8 @@ in emacs.overrideAttrs (o: {
   # Recent tip of trunk
   src = fetchFromSavannah {
     repo = "emacs";
-    rev = "1682bd18f50ed2095b2162207603c6b8a3f0225f";
-    hash = "sha256-ZsH2hWkMV7/fDNrG6cE8rRudhHfNvJs+CbJnKMLnVMA";
+    rev = "39e0c60176242a2ca09f65090bcf2751b346ed26";
+    hash = "sha256-M3Pf3AicylpqkQrUGLPsrLA+Gim8WoErpWxb/xUewkQ=";
   };
 
   version = "29.0.50";
